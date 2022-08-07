5bb80b2861e5ad91502c2315be25d09e51e308b5
jiah <17723575357@163.com>
2022-08-07 19:31:37 +0800

    chore:解决编码问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,7 +11,8 @@ import { sidebar } from './utils/sidebar'
 const config = defineConfig({
   title: 'CoderJ',
   titleTemplate: 'CoderJ',
-  base:'/',
+  // base:'/',
+  base:'/CoderJBlog/',
   outDir:'../dist',
   head: [
     ['link', { rel: 'icon', href: '/images/iconfont.png', crossorigin: '' }]
