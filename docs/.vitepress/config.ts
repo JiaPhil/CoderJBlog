278c2e9ed3396fa8b5b093b0ebd283c033708c59
jiah <jiah@elane.cn>
2022-08-08 15:35:00 +0800

    chore: 修改标签页icon路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'vitepress'
 import { nav } from './utils/nav'
 import { sidebar } from './utils/sidebar'
 
-/**
- * 参考：https://vitepress.vuejs.org/config/app-configs.html
- * 
- * 修改这里需要重启项目才可以生效
- */
+
 
 const config = defineConfig({
   title: 'CoderJ',
